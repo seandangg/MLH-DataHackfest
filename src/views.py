72ed3725b55a5b944e82ce86fb4bd8e481202744
nguyenhethan1 <nguyenhethan1@gmail.com>
2024-05-22 13:33:09 -0700

    css

--- src/views.py
+++ src/views.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
-views = Blueprint(__name__, "views")
+from flask import Blueprint, render_template, request
+views = Blueprint(__name__, 'views')
 
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html', name='Joe')
+
+
+@views.route('/profile/<username>')
+def profile(username):
+    args = request.args
+    name = args.get('name')
+    return render_template('index.html', name=username)
