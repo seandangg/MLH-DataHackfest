4f7cefb69840038a082f0728b628c0248b9d1a9b
nguyenhethan1 <nguyenhethan1@gmail.com>
2024-05-22 13:32:57 -0700

    merge

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from views import views
+import pandas as pd
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+
+
+df = pd.read_csv('pvc.csv')
+
+
+def index():
+    return render_template(index.html)
